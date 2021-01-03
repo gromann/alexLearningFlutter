a267fd9062a56df6a670a17fcf8e21189de3602c
Alexander Grossmann <alexander.grossmann@smartbricks.at>
2021-01-03 22:29:12 +0100

    add stateless widget to enable hot reload and make everything more modular

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,16 @@ import 'package:flutter/material.dart';
 
 void main() =>
     // a wrapper for all the material design stuff
-    runApp(MaterialApp(
-        home: Scaffold(
+    runApp(MaterialApp(home: stateless()));
+
+// stateless widget to enable hot reload
+// every widget is just a class
+// makes code more reusable
+class stateless extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
       appBar: AppBar(
         title: Text('my first app'),
         centerTitle: true,
@@ -11,7 +19,7 @@ void main() =>
       ),
       body: Center(
         child: Text(
-          'Hier bin ich',
+          'Hier bin ich!',
           style: TextStyle(
               fontSize: 20.0,
               fontWeight: FontWeight.bold,
@@ -24,15 +32,6 @@ void main() =>
         onPressed: () {},
         backgroundColor: Colors.redAccent,
       ),
-    )));
-
-// stateless widget to enable hot reload
-// every widget is just a class
-// makes code more reusable
-class stateless extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return container();
+    );
   }
 }
